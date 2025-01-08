b956ee4e311eacaa1af2c946c6aa877a13eb5617
Sarmad Shabbir <sshabbir@avanceon.ae>
2025-01-08 11:36:46 +0500

    Updated Meals App

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,33 +1,33 @@
-import { FlatList, View, Text } from "react-native"
+import { FlatList } from 'react-native';
+import CategoryGridTile from '../components/CategoryGridTile';
 
-import { CATEGORIES } from "../data/dummy-data"
-import CategoryGridTile from "../components/CategoryGridTile";
-import Category from "../models/category";
+import { CATEGORIES } from '../data/dummy-data';
 
-function CategoriesScreen ({ navigation }) {
-    function renderCategoryItem(itemData) {
-        function pressHandler() {
-            navigation.navigate('MealsOverview', {
-                categoryId: itemData.item.id,
-            });
-        }
-    
+function CategoriesScreen({ navigation }) {
+  function renderCategoryItem(itemData) {
+    function pressHandler() {
+      navigation.navigate('MealsOverview', {
+        categoryId: itemData.item.id,
+      });
+    }
+
     return (
-        <CategoryGridTile 
-            title={itemData.item.title} 
-            color={itemData.item.color} 
-            onPress={pressHandler}    
-        />
+      <CategoryGridTile
+        title={itemData.item.title}
+        color={itemData.item.color}
+        onPress={pressHandler}
+      />
     );
+  }
+
+  return (
+    <FlatList
+      data={CATEGORIES}
+      keyExtractor={(item) => item.id}
+      renderItem={renderCategoryItem}
+      numColumns={2}
+    />
+  );
 }
 
-    return (
-        <FlatList
-            data={CATEGORIES}
-            keyExtractor={item => item.id}
-            renderItem={renderCategoryItem}
-            numColumns={2}
-        />
-    )
-}
-export default CategoriesScreen
+export default CategoriesScreen;
